17e55175f5a4b4a25084190f2470bc0459a2a271
anass mouzoun <anassmouzoun12@gmail.com>
2025-04-22 09:42:15 +0000

    staging_models

---
+++ dbt_retail/models/staging/stg_customers.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT
+  customer_id,
+  customer_unique_id,
+  customer_zip_code_prefix,
+  customer_city,
+  customer_state
+FROM {{ source('bronze', 'customers') }}
